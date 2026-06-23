b6de933c629edf3ff28f6ca6edb4463f596e447d
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-06-23 23:47:21 +0900

    fix: align foundation outputs with latest stack

--- 00-foundation/outputs.tf
+++ 00-foundation/outputs.tf
@@ -10,6 +10,6 @@ output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.this.id
 }
 
-output "log_analytics_workspace_name" {
-  value = azurerm_log_analytics_workspace.this.name
+output "tags" {
+  value = var.tags
 }
