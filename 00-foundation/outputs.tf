40b0c1509494350c16ec9e39a403001f1669671f
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-06-23 23:33:16 +0900

    feat: add foundation outputs

---
+++ 00-foundation/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "location" {
+  value = azurerm_resource_group.this.location
+}
+
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.this.id
+}
+
+output "log_analytics_workspace_name" {
+  value = azurerm_log_analytics_workspace.this.name
+}
